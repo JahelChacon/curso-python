ce7f41eb0731832e89b3dab146de8490dc284129
jahelchacon <jahelchacon@gmail.com>
2023-09-14 21:02:53 -0600

    Se agrega el archivo types de la clase 3 de tipos de datos

---
+++ 03_types.py
@@ -0,0 +1,22 @@
+# String 
+nombre = "Jahel"
+nombre = 'Jahel'
+print('Mi nombre -> ', nombre)
+
+# Ver el tipo de la variable
+print(type(nombre))
+
+# int 
+numero = 20
+print('Mi numero -> ', numero)
+print(type(numero))
+
+# boolean
+boolean = False
+print('Mi booleano -> ', boolean)
+print(type(boolean))
+
+# Inputs
+edad = input("Cual es tu edad?")
+print("La edad es -> ", edad)
+print(type(edad))
