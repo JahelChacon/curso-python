d85eb55df12538e9ae846d9fa67354a5e6d1f8af
jahelchacon <jahelchacon@gmail.com>
2023-09-14 21:38:10 -0600

    Se agrega el archivo 5 de numbers para la clase 5

---
+++ 05_numbers.py
@@ -0,0 +1,14 @@
+lives = 3
+edad = 25
+budget = 7000
+temperatura = 23.2
+print(lives, edad, budget, temperatura)
+
+lives = lives -1
+print('Vidas -> ',lives)
+
+lives += 2
+print('Vidas -> ',lives)
+
+number = 1000000000000000000000.1
+print(number)
