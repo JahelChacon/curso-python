445b75bcaea77b502179c139db1a7ba55e683ade
jahelchacon <jahelchacon@gmail.com>
2023-09-14 21:55:34 -0600

    Se agrega el archivo 7 de transformacion de tipos de datos.
    Tambien se termina el modulo de tipos de datos

---
+++ 07_transformarTipos.py
@@ -0,0 +1,10 @@
+name = "Jahel"
+print(name + " " + str(12))
+edad = input("Cual es tu edad?")
+print(f"Tu edad en 10 años será {int(edad) + 10} ")
+print(type(int(edad)))
+
+## Otra forma
+edad2 = input("Cual es tu edad hace 10 años?")
+edad2 = int(edad2) + 10
+print("Tu edad actual es", edad2)
