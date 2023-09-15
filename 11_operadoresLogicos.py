94e6c9d33edd2a4fd097cf58d5efc7ad9e972873
jahelchacon <jahelchacon@gmail.com>
2023-09-14 23:56:12 -0600

    Se agrega la clase 11 de operadores logicos

---
+++ 11_operadoresLogicos.py
@@ -0,0 +1,16 @@
+# AND 
+print("### AND")
+print('True AND True =>', True and True) # TRUE
+print('True AND False =>', True and False) # FALSE
+print( 4 > 2 and 2 > 1) # TRUE
+print( 4 > 2 and 0 > 1) # FALSE
+
+print("*"*30)
+
+# OR
+print("### OR")
+print('True OR True =>', True or True) # TRUE
+print('True OR False =>', True or False) # TRUE
+print('False OR False =>', False or False) # FALSE
+
+print("*"*30)
