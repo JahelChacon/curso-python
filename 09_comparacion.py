c8d42201f82285188ebd6ac259beac7eb5153e3d
jahelchacon <jahelchacon@gmail.com>
2023-09-14 23:39:57 -0600

    Se agrega la clase 9 de comparadores

---
+++ 09_comparacion.py
@@ -0,0 +1,28 @@
+# > mayor que
+print("Mayor que:",7 > 3) # True
+
+# < Menor que
+print("Menor que:",7 < 3) # False
+
+# >= Mayor o igual
+print("Mayor o igual")
+print(7 > 7) # False
+print(7 >=7) # True
+
+# <= Menor o igual
+print("Menor o igual")
+print(7 < 7) # False
+print(7 <=7) # True
+
+# == Iguales
+print("Iguales")
+print(6 == 6) # True
+print(6 == 3) # False
+print("Apple" == 'Apple') # True
+print("Apple" == 'apple') # False
+
+# != Diferentes
+print("Diferentes")
+print(6 != 3) # True
+print(6 != 6) # False
+
