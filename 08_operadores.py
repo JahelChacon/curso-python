5089109f1728bd2f9ffefcd5ee62e09a803186e3
jahelchacon <jahelchacon@gmail.com>
2023-09-14 23:30:25 -0600

    Se agrega el archivo 8 de operadores para la clase 8

---
+++ 08_operadores.py
@@ -0,0 +1,14 @@
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 2)
+print(10 % 2) # Mod
+print("Division:",10 / 3)
+print("Solo entero:",10 // 3) # Dividir y dejar solo el entero
+print("Elevado", 2 ** 3) # 2 elevado a la 3
+
+
+
+
+print('Hola ' + 'mundo')
+print('Hola ' * 3)
