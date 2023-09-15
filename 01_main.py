53c2852f20de8155fb6466f294f019d196e9eee8
jahelchacon <jahelchacon@gmail.com>
2023-09-14 20:51:52 -0600

    Arreglando el primer archivo. Cambiando el nombre a main 01

---
+++ 01_main.py
@@ -0,0 +1,19 @@
+print("Hola Bienvenido al curso de python")
+
+print("Este es un print")
+
+# Estas son operaciones 
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+
+# Esto es un comentario
+
+"""
+Hacer un comentario de varias lineas 
+"""
+
+'''
+Tambien funciona asi
+'''
